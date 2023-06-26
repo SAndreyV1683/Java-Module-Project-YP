7c051aa83f954711d25e05d78b478b14688c35d0
Andrey <a.sboev@teleswyz.ru>
2023-06-26 22:00:16 +0300

    Входные параметры для счётчика

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("На скольких человек необходимо разделить счёт? \nВведите значение:");
+        Scanner scanner = new Scanner(System.in);
+        int peopleCount = 0;
+        boolean isNumber = false;
+
+        while (!isNumber) {
+            if (scanner.hasNextInt()) {
+                peopleCount = scanner.nextInt();
+                if (peopleCount < 1) {
+                    System.out.println("Это некорректное значение для подсчёта. \nВведите значение заново.");
+                    scanner = new Scanner(System.in);
+                } else {
+                    isNumber = true;
+                }
+            } else {
+                System.out.println("Введите число");
+                scanner = new Scanner(System.in);
+            }
+        }
+
+        System.out.println("peopleCount " +  peopleCount);
     }
 }
